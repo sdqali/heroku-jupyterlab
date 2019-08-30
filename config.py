53c37574d5851b1e0fb5c4dd6f0adc2adab04499
Sadique Ali Koothumadan <sadiqalikm@gmail.com>
2019-08-29 22:10:46 -0700

    Adjust config for pgcontents

--- config.py
+++ config.py
@@ -1,30 +1,21 @@
 import os
 
-from s3contents import S3ContentsManager
-
-try:
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-except (NameError, KeyError):
-    print('Using Bucketeer instead…')
-    # Bucketeer addon.
-    AWS_ACCESS_KEY_ID = os.environ['BUCKETEER_AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['BUCKETEER_AWS_SECRET_ACCESS_KEY']
-    S3_BUCKET_NAME = os.environ['BUCKETEER_BUCKET_NAME']
+from pgcontents import PostgresContentsManager
 
 PASSWORD = os.environ.get('PASSWORD', '')
 
-# c = get_config()
-c.NotebookApp.contents_manager_class = S3ContentsManager
-c.S3ContentsManager.access_key_id = AWS_ACCESS_KEY_ID
-c.S3ContentsManager.secret_access_key = AWS_SECRET_ACCESS_KEY
-c.S3ContentsManager.bucket = S3_BUCKET_NAME
+
+c = get_config()
+c.NotebookApp.contents_manager_class = PostgresContentsManager
 
 c.NotebookApp.token = ''
 c.NotebookApp.password = PASSWORD
 
+database_url = os.getenv('DATABASE_URL', None)
+print("database url " + database_url)
+c.PostgresContentsManager.db_url = database_url
 
+c.PostgresContentsManager.user_id = 'heroku'
 # Configuration file for jupyterhub.
 
 #------------------------------------------------------------------------------
